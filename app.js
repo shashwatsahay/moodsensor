ba964c63dd53990a62dc57ede8be680763b6bf98
Shashwat Sahay <virgo30892@gmail.com>
2018-10-26 14:59:39 +0200

    Added Comments

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+/* Global variables decrlaing the values of heatz Rate and the modd status
+*/
 var statusText = document.querySelector('#statusText');
 var moodText = document.querySelector('#moodText');
 
+/*
+Listener to changes heart rate sensor
+*/
 statusText.addEventListener('click', function() {
   statusText.textContent = 'Breathe...';
   moodText.textContent = 'Relaxed';
@@ -11,7 +16,9 @@ statusText.addEventListener('click', function() {
     statusText.textContent = error;
   });
 });
-
+/*
+The function is directly responsible changes of UI values 
+*/
 function handleHeartRateMeasurement(heartRateMeasurement) {
   heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
     var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
@@ -21,6 +28,10 @@ function handleHeartRateMeasurement(heartRateMeasurement) {
   });
 }
 
+/*
+The function updates the heartRate class values i.e. it updates the last 10 heart rate vlaues
+amd and the state of the person
+*/
 function getMeasurments(){
 	heartRates.get();
 
